68d16004a14afb9bb13473da71ae76f3ff981937
refaat al ktifan <Refaat@alktifan.com>
2026-01-10 22:07:19 +0100

    feat(extractors): add extractor registry service
    - Create central registry for all extractors
    - Implement extractor discovery and instantiation
    - Add extractor interface definitions

---
+++ src/extractors/index.ts
@@ -0,0 +1,63 @@
+export * from '../types/index';
+
+export {
+    ExtractorRegistryService,
+    ExtractorRegistrationCallback,
+    ExtractorMetadata as RegistryExtractorMetadata,
+    ExtractorInfo,
+} from './extractor-registry.service';
+
+export { HttpApiExtractor } from './http-api';
+export { HttpApiExtractorConfig } from './http-api';
+
+export { DatabaseExtractor } from './database';
+export { DatabaseExtractorConfig } from './database';
+
+export { S3Extractor } from './s3';
+export { S3ExtractorConfig } from './s3';
+
+export { FtpExtractor } from './ftp';
+export { FtpExtractorConfig } from './ftp';
+
+export { FileExtractor } from './file';
+export { FileExtractorConfig } from './file';
+
+export { VendureQueryExtractor } from './vendure-query';
+export { VendureQueryExtractorConfig, VendureQueryFilter } from './vendure-query';
+
+export { WebhookExtractor } from './webhook';
+export { WebhookExtractorConfig } from './webhook';
+
+export { GraphQLExtractor } from './graphql';
+export { GraphQLExtractorConfig, GraphQLPaginationConfig } from './graphql';
+
+import { HttpApiExtractor } from './http-api';
+import { DatabaseExtractor } from './database';
+import { S3Extractor } from './s3';
+import { FtpExtractor } from './ftp';
+import { FileExtractor } from './file';
+import { VendureQueryExtractor } from './vendure-query';
+import { WebhookExtractor } from './webhook';
+import { GraphQLExtractor } from './graphql';
+
+export const BUILT_IN_EXTRACTOR_CLASSES = [
+    HttpApiExtractor,
+    WebhookExtractor,
+    VendureQueryExtractor,
+    FileExtractor,
+    FtpExtractor,
+    S3Extractor,
+    DatabaseExtractor,
+    GraphQLExtractor,
+] as const;
+
+export const BUILT_IN_EXTRACTORS = [
+    'HttpApiExtractor',
+    'WebhookExtractor',
+    'VendureQueryExtractor',
+    'FileExtractor',
+    'FtpExtractor',
+    'S3Extractor',
+    'DatabaseExtractor',
+    'GraphQLExtractor',
+] as const;
