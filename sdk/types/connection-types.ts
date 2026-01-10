282894610db745c098e7b210d1748323f7707e67
refaat al ktifan <Refaat@alktifan.com>
2026-01-10 22:11:29 +0100

    chore: add SDK exports and configuration files
    - Add SDK standalone exports
    - Include analytics utilities
    - Add feed templates
    - Include export configurations
    - Add Vendure schemas
    - Add package-lock.json

---
+++ sdk/types/connection-types.ts
@@ -0,0 +1,4 @@
+/**
+ * Re-export connection types from src for dashboard access
+ */
+export * from '../../src/sdk/types/connection-types';
---
+++ vendure-schemas/vendure-entity-schemas.ts
@@ -0,0 +1,4 @@
+/**
+ * Re-export vendure entity schemas from src for dashboard access
+ */
+export * from '../src/vendure-schemas/vendure-entity-schemas';
